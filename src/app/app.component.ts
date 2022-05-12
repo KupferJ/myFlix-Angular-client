911f23db686e180239a33ada2d71ac2b21b36693
KupferJ <95375522+KupferJ@users.noreply.github.com>
2022-05-12 12:16:55 +0800

    added welcome page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { MatDialog } from '@angular/material/dialog';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -11,27 +7,43 @@ import { MovieCardComponent } from './movie-card/movie-card.component';
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
-
-  constructor(public dialog: MatDialog) { }
-  //function to open the dialog when the signup button is clicked  
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-    //assign width to the dialog
-    width: '280px'
-    });
-  }
-
-  //function to open the dialog when the login button is clicked
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-      width: '280px'
-    });
-  }
-
-  //movie card with all movies in it
-  openMoviesDialog(): void {
-    this.dialog.open(MovieCardComponent, {
-      width: '500px'
-    });
-  }
 }
+
+
+// import { Component } from '@angular/core';
+// import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+// import { MatDialog } from '@angular/material/dialog';
+// import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+// import { MovieCardComponent } from './movie-card/movie-card.component';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.scss']
+// })
+// export class AppComponent {
+//   title = 'myFlix-Angular-client';
+
+//   constructor(public dialog: MatDialog) { }
+//   //function to open the dialog when the signup button is clicked  
+//   openUserRegistrationDialog(): void {
+//     this.dialog.open(UserRegistrationFormComponent, {
+//     //assign width to the dialog
+//     width: '280px'
+//     });
+//   }
+
+//   //function to open the dialog when the login button is clicked
+//   openUserLoginDialog(): void {
+//     this.dialog.open(UserLoginFormComponent, {
+//       width: '280px'
+//     });
+//   }
+
+//   //movie card with all movies in it
+//   openMoviesDialog(): void {
+//     this.dialog.open(MovieCardComponent, {
+//       width: '500px'
+//     });
+//   }
+// }
