6c00ba45009af254d650092126cc4f6cbd510230
KupferJ <95375522+KupferJ@users.noreply.github.com>
2022-05-12 12:21:50 +0800

    added routing
    -added necessary routing
    -added new imports for new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,18 +15,47 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from'@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+
+//components
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { DirectorViewComponent } from './director-view/director-view.component';
+import { GenreViewComponent } from './genre-view/genre-view.component';
+import { SynopsisCardComponent } from './synopsis-card/synopsis-card.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { UserFavoritesComponent } from './user-favorites/user-favorites.component';
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent },
+  { path: 'movies', component: MovieCardComponent },
+  { path: 'profile', component: UserProfileComponent },
+  { path: 'favorites', component: UserFavoritesComponent},
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     UserRegistrationFormComponent,
     UserLoginFormComponent,
-    MovieCardComponent
+    MovieCardComponent,
+    WelcomePageComponent,
+    DirectorViewComponent,
+    GenreViewComponent,
+    SynopsisCardComponent,
+    NavBarComponent,
+    UserProfileComponent,
+    UserFavoritesComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),  
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
@@ -37,7 +66,9 @@ import { MovieCardComponent } from './movie-card/movie-card.component';
     MatFormFieldModule,
     MatDialogModule,
     MatSnackBarModule,
-    FormsModule
+    FormsModule,
+    MatIconModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
