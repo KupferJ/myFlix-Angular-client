f92b13b478679e960a03a0f9e3fa20c08e3b6641
KupferJ <95375522+KupferJ@users.noreply.github.com>
2022-05-12 12:19:38 +0800

    added navigation bar

---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss']
+})
+
+export class NavBarComponent implements OnInit {
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public router: Router,
+    public snackBar: MatSnackBar,
+
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  toMovies(): void {
+    this.router.navigate(['movies']);
+  }
+
+  toProfile(): void {
+    this.router.navigate(['profile']);
+  }
+  toFavorites(): void {
+    this.router.navigate(['favorites']);
+  }
+
+  logoutFunction(): void {
+    localStorage.clear();
+    this.snackBar.open('You successfully logged out!', 'OK', {
+      duration: 2000,
+    });
+    this.router.navigate(['welcome']);
+  }
+
+}
